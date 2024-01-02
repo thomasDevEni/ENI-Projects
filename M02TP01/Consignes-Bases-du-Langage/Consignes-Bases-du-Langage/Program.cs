6fbc02d9fc48d490681a5f96ff5d06cc6da3b0ee
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-02 15:10:34 +0100

    Ajout des classes Forme & Cercle

--- M02TP01/Consignes-Bases-du-Langage/Consignes-Bases-du-Langage/Program.cs
+++ M02TP01/Consignes-Bases-du-Langage/Consignes-Bases-du-Langage/Program.cs
@@ -6,13 +6,36 @@ namespace Consignes_Bases_du_Langage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Classe de base pour les formes géométriques
+            public abstract class Forme
+                {
+                    public abstract double CalculerSurface();
+                    public override string ToString()
+                    {
+                        return $"Surface: {CalculerSurface()} unités carrées";
+                    }
+                }
+            // Classe représentant un cercle
+            public class Cercle : Forme
+            {
+                public double Rayon { get; set; }
+
+                public override double CalculerSurface()
+                {
+                    return Math.PI * Rayon * Rayon;
+                }
+
+                public override string ToString()
+                {
+                    return $"Cercle - {base.ToString()}";
+                }
+            }
             var formes = new List<Forme> {
-            new Cercle { Rayon = 3 },
-            new Rectangle { Longueur = 4, Largeur = 3 },
-            new Carre { Longueur = 3 },
-            new Triangle { A = 4, B = 5, C = 6 }
-        };
+                new Cercle { Rayon = 3 },
+                new Rectangle { Longueur = 4, Largeur = 3 },
+                new Carre { Longueur = 3 },
+                new Triangle { A = 4, B = 5, C = 6 }
+            };
             foreach (Forme forme in formes)
             {
                 Console.WriteLine(forme);
