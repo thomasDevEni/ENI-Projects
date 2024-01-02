cc11e28405ef48e376762d2785b362a2fdb7ce3d
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-02 16:29:28 +0100

    01

---
+++ M02TP01/BatailleCorseApp/BatailleCorseApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BatailleCorseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
