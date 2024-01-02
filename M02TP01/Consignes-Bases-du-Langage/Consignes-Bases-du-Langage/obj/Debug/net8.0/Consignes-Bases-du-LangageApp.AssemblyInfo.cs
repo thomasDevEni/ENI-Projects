14a2e9140cee2cacae5c5107fa5974a4c3d9fcae
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-02 16:23:40 +0100

    xoxo

--- M02TP01/Consignes-Bases-du-Langage/Consignes-Bases-du-Langage/obj/Debug/net8.0/Consignes-Bases-du-LangageApp.AssemblyInfo.cs
+++ M02TP01/Consignes-Bases-du-Langage/Consignes-Bases-du-Langage/obj/Debug/net8.0/Consignes-Bases-du-LangageApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consignes-Bases-du-LangageApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a320dbc02a8d17040aa5fc2fad888dc48f8abb40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6fb2c8d9e2d3699b1b6bc9de72956b2f856fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consignes-Bases-du-LangageApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consignes-Bases-du-LangageApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
