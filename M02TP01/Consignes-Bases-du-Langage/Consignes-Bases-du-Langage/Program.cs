1172ed3fa0265ae85920d06ceaa1dba7011c587f
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-02 14:52:53 +0100

    ajout tp2

---
+++ M02TP01/Consignes-Bases-du-Langage/Consignes-Bases-du-Langage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Consignes_Bases_du_Langage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var formes = new List<Forme> {
+            new Cercle { Rayon = 3 },
+            new Rectangle { Longueur = 4, Largeur = 3 },
+            new Carre { Longueur = 3 },
+            new Triangle { A = 4, B = 5, C = 6 }
+        };
+            foreach (Forme forme in formes)
+            {
+                Console.WriteLine(forme);
+            }
+        }
+    }
+}
