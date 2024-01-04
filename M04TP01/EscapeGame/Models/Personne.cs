84fbff596bda9e0690adf91bb6d26b7ef3bfb15d
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 16:38:06 +0100

    ajout relations vues personnes

---
+++ M04TP01/EscapeGame/Models/Personne.cs
@@ -0,0 +1,12 @@
+﻿namespace EscapeGame.Models
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public int Age { get; set; }
+
+    }
+}
