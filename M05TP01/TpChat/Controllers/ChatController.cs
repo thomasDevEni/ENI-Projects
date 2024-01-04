7f96e044015e6b28ece3ccd47bc36186624b0e88
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 18:55:31 +0100

    ajout data tp

---
+++ M05TP01/TpChat/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TpChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: ChatController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ChatController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+        // GET: ChatController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ChatController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
