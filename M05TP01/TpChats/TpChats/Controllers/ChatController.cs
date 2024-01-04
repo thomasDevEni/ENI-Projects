30bd26c37317992c1bbcb604338355c9b98f10d2
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 17:00:40 +0100

    modif1 chatchat

---
+++ M05TP01/TpChats/TpChats/Controllers/ChatController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TPChats.Models;
+
+namespace TpChats.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: ChatController
+        public ActionResult Index()
+        {
+             var v = Chat.GetMeuteDeChats();
+            return View(v);
+        }
+
+        // GET: ChatController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+
+        // GET: ChatController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ChatController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
