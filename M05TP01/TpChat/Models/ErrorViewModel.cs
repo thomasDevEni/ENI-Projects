3d798442f13195711cb4f15de9f955f99f82cdcd
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 18:41:56 +0100

    ajout tp 01 module 05

---
+++ M05TP01/TpChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TpChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
