e6d0fd351005684d40a3392ed0e507913b4bd247
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 14:13:33 +0100

    ajout demo razor

---
+++ MyFirstMVC/Module05-Razor/Module05-Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module05_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
