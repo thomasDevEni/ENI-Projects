b6ca6f80e85af6cf38785a14a6267685ef5e5f7a
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 11:01:55 +0100

    modif test webapp

--- MyFirstMVC/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ MyFirstMVC/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423af4a589e3e5e998c7f14455a44fa6577d32c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7daf5ae907a1f2a34c037d13fbc509b0d99faef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
