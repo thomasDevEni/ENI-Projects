432c8c9f99d6556bcf9a504e66caed852d245a1e
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-01-04 11:55:27 +0100

    resolution exo

--- M04TP01/EscapeGame/Controllers/HomeController.cs
+++ M04TP01/EscapeGame/Controllers/HomeController.cs
@@ -14,6 +14,10 @@ namespace EscapeGame.Controllers {
             return View();
         }
 
+        public IActionResult Suite()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ M04TP01/EscapeGame/Controllers/MachinController.cs
@@ -0,0 +1,51 @@
+﻿using EscapeGame.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EscapeGame.Controllers
+{
+    public class MachinController : Controller
+    {
+        private readonly ILogger<MachinController> _logger;
+
+        public MachinController(ILogger<MachinController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Truc()
+        {
+            return View();
+        }
+
+        public IActionResult Bidule(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult Jo()
+        {
+            return View();
+        }
+
+        [Route("/A/21/B/23/C")]
+        public IActionResult Fin(int v1=161, int v2=3)
+        {
+            ViewBag.Valeur = v1 * v2;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
