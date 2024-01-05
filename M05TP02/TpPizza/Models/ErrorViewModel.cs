61451f163d603ee59fa93aa0339b1c6901e1e1f5
thomasDevEni <thomas.requier2023@campus-eni.fr>
2024-01-05 14:18:00 +0100

    ajout tppizza MVC

---
+++ M05TP02/TpPizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TpPizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ M05TP02/TpPizza/Models/Ingredient.cs
@@ -0,0 +1,6 @@
+﻿namespace BO {
+    public class Ingredient {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+}
