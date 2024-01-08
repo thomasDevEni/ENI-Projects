e2ec8a6ea182bda86bb01caf7e9e716c4d4d6476
thomasDevEni <thomas.requier2023@campus-eni.fr>
2024-01-08 11:24:23 +0100

    ajout tag helper submit

---
+++ M05TP02/TpPizza/TagHelper/SubmitTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TpPizza.TagHelpers
+{
+    public class SubmitTagHelper : TagHelper
+    {
+
+        public string? Label { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "input";
+            output.Attributes.SetAttribute("type", "submit");
+            if (Label != null)
+            {
+                output.Attributes.SetAttribute("value", Label);
+            }
+        }
+    }
+}
