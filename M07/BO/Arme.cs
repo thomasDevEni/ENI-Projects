6295958b7b626b388f4d9ee9b0eab1005a9385c5
thomasDevEni <thomas.requier2023@campus-eni.fr>
2024-01-09 16:41:50 +0100

    ajout armes

---
+++ M07/BO/Arme.cs
@@ -0,0 +1,9 @@
+﻿namespace TpDojo.Models
+{
+    public class Arme
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Degats { get; set; }
+    }
+}
