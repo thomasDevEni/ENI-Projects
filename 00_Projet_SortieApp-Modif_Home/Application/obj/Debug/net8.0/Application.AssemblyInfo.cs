44e4ec9b6bb6516c6f11ed8922a7907abba9fc3c
Thomas REQUIER <thomas.requier2023@campus-eni.fr>
2024-02-13 21:28:59 +0100

    ok

--- 00_Projet_SortieApp-Modif_Home/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ 00_Projet_SortieApp-Modif_Home/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974f38714c92114b13db97cfef94118e6cf1aeee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ac10c461306ea29419d1810ffca51ee858e4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 00_Projet_SortieApp-Modif_Home/Sortie/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ 00_Projet_SortieApp-Modif_Home/Sortie/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5d55e6c49eaf641b22803a278f42052d6393d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ac10c461306ea29419d1810ffca51ee858e4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
